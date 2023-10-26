abadbfd5e1bc605d2416e14b016b6b01080f50a1
user <user@LEENASRI>
2023-10-26 21:44:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IStudent
+    {
+        void print();//we cannot implement interface method.it is by public so they will not have access specifier.
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
